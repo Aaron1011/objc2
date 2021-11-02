madsmtm/rust-objc

Test that having no return type on msg_send doesn't compile

tests/ui/msg_send_no_return_type.rs
@@ -1,0 +1,8 @@
+use objc::{class, msg_send};
+
+fn main() {
+    unsafe {
+        let cls = class!(NSObject);
+        let _obj = msg_send![cls, new];
+    }
+}

Aaron1011/objc2

objc2/tests/ui/msg_send_no_return_type.rs
@@ -1,0 +1,8 @@
+use objc2::{class, msg_send};
+
+fn main() {
+    unsafe {
+        let cls = class!(NSObject);
+        let _obj = msg_send![cls, new];
+    }
+}