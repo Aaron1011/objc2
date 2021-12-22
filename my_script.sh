b771630a64fc3f871e1acd1fb397d31402a58d1a
Aaron Hill <aa1ronham@gmail.com>
2021-12-22 11:38:07 -0500

    Fetch other commit

--- my_script.sh
+++ my_script.sh
@@ -1,5 +1,6 @@
 set -euxo pipefail
 
+git fetch origin 242e218400c16071c42a21024229d629a511beaa
 cargo test --no-fail-fast --no-default-features
 git checkout 242e218400c16071c42a21024229d629a511beaa
 cargo test --no-fail-fast --no-default-features
