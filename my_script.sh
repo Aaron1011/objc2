b5413fea4fbff31556c00981b4626291358a12ee
Aaron Hill <aa1ronham@gmail.com>
2021-12-22 11:48:49 -0500

    Use only one job when triggering ICE

--- my_script.sh
+++ my_script.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 git fetch origin 242e218400c16071c42a21024229d629a511beaa
 cargo test --no-fail-fast --no-default-features
 git checkout 242e218400c16071c42a21024229d629a511beaa
-cargo test --no-fail-fast --no-default-features
+cargo test --no-fail-fast --no-default-features --jobs 1
